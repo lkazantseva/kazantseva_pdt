ed646c31ad0918be99f61c3069d33f3830faab35
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-04-23 22:37:01 +0700

    фикс теста Point

--- sandbox/src/test/java/ru/kaz/pdt/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/kaz/pdt/sandbox/PointTests.java
@@ -6,9 +6,10 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-
   public void testDistance() {
-    Point P = new Point (2,1,2,6);
-    Assert.assertEquals(P.distance(), 5.0);
+    Point P1 = new Point (0,1);
+    Point P2 = new Point(2, -2);
+
+    Assert.assertEquals(P1.distance(P2), 3.605551275463989);
   }
 }
