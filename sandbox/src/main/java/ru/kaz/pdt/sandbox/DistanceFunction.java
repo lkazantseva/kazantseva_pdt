47aa450cbcfa6425d057a5118819d7d98053708e
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-04-23 22:35:16 +0700

    фикс вычисления расстояния с помощью метода

--- sandbox/src/main/java/ru/kaz/pdt/sandbox/DistanceFunction.java
+++ sandbox/src/main/java/ru/kaz/pdt/sandbox/DistanceFunction.java
@@ -2,8 +2,9 @@ package ru.kaz.pdt.sandbox;
 
 public class DistanceFunction {
   public static void main(String[] args) {
-    Point P = new Point(2,1,2,6);
-
-    System.out.println("Расстояние между двумя точками(" + P.x1 + ","+ P.y1 + ") и (" + P.x2 + "," + P.y2 + ") =" + P.distance());
+    Point P1 = new Point (0,1);
+    Point P2 = new Point(2, -2);
+    System.out.println("Расстояние между двумя точками(" + P1.x + "," + P1.y + ") и" + "(" + P2.x + ", " + P2.y + ") =" + P1.distance(P2));
   }
-}
+  }
+
--- sandbox/src/main/java/ru/kaz/pdt/sandbox/Point.java
+++ sandbox/src/main/java/ru/kaz/pdt/sandbox/Point.java
@@ -1,19 +1,19 @@
 package ru.kaz.pdt.sandbox;
 
 public class Point {
-  public double x1;
-  public double x2;
-  public double y1;
-  public double y2;
+  public double x;
+  public double y;
 
-  public Point(double x1, double x2, double y1, double y2) {
-    this.x1 = x1;
-    this.x2 = x2;
-    this.y1 = y1;
-    this.y2 = y2;
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
   }
-  public double distance() {
-    return Math.sqrt((this.y1-this.x1)*(this.y1-this.x1)+(this.y2-this.x2)*(this.y2-this.x2));
+
+  public double distance(Point P) {
+    return Math.sqrt((P.x - this.x)*(P.x-this.x)+(P.y-this.y)*(P.y-this.y));
   }
 }
+
+
 
