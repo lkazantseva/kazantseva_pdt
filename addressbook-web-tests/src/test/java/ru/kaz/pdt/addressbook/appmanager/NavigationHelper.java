645079e21ccce0be07dcb09b62fb5e1022c50bd9
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-04-28 01:55:36 +0700

    Оптимизация переходов между страницами

--- addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/appmanager/NavigationHelper.java
@@ -10,14 +10,26 @@ public class NavigationHelper extends BaseHelper {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void returnToHomePage() {
+    if (isElementPresent(By.id ("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 
   public void gotoAddNewContactPage() {
+    if (isElementPresent(By.id("content"))
+            && isElementPresent(By.name("theform"))) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 }
