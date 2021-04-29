388e0474d43ea1ed234da106aed3864fa155716c
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-04-29 17:23:55 +0700

    Добавила предусловие создания группы для тестов ContactCreation, ContactDeletion, ContactModification

--- addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/ContactCreationTests.java
@@ -2,10 +2,15 @@ package ru.kaz.pdt.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.kaz.pdt.addressbook.model.ContactData;
+import ru.kaz.pdt.addressbook.model.GroupData;
 
 public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getNavigationHelper().gotoAddNewContactPage();
     app.getContactHelper().createContact(new ContactData("Ivan", "Ivanov", "89094567898", "ivanovivan@yandex.ru", "test1"), true);
     app.getNavigationHelper().returnToHomePage();
--- addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/ContactDeletionTests.java
@@ -2,12 +2,18 @@ package ru.kaz.pdt.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.kaz.pdt.addressbook.model.ContactData;
+import ru.kaz.pdt.addressbook.model.GroupData;
 
-public class ContactDeletionTests extends TestBase{
+public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() throws Exception {
-    if(! app.getContactHelper().isThereAContact()) {
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
+    app.getNavigationHelper().returnToHomePage();
+    if (!app.getContactHelper().isThereAContact()) {
       app.getNavigationHelper().gotoAddNewContactPage();
       app.getContactHelper().createContact(new ContactData("Ivan", "Ivanov", "89094567898", "ivanovivan@yandex.ru", "test1"), true);
       app.getNavigationHelper().returnToHomePage();
