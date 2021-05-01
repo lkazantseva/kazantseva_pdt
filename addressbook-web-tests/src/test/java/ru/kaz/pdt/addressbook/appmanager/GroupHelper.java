6875e6fb8000a85ee19dbe9cd5563aef6145e1e4
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-05-01 15:08:23 +0700

    Формирование коллекции в тестах для групп

--- addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/appmanager/GroupHelper.java
@@ -2,17 +2,24 @@ package ru.kaz.pdt.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.kaz.pdt.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends BaseHelper {
   public GroupHelper(WebDriver wd) {
     super(wd);
   }
+
   public void returnToGroupPage() {
     click(By.linkText("group page"));
   }
 
-  public void submitGroupCreation() { click(By.name("submit")); }
+  public void submitGroupCreation() {
+    click(By.name("submit"));
+  }
 
   public void fillGroupForm(GroupData groupData) {
     type(By.name("group_name"), groupData.getName());
@@ -53,5 +60,16 @@ public class GroupHelper extends BaseHelper {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
