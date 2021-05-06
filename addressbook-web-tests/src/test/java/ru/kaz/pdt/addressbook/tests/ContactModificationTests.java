4ec6b1f38c7201306c89f553eb66307e00c9236e
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-05-06 23:48:19 +0700

    ContactTests Сортировка списков

--- addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/ContactModificationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.kaz.pdt.addressbook.model.ContactData;
 import ru.kaz.pdt.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -23,17 +23,20 @@ public class ContactModificationTests extends TestBase {
       app.getNavigationHelper().returnToHomePage();
     }
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() -1);
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData(before.get(before.size()-1).getId(),"Petr", "Petrov", "89097865555", "petrpetrov@yandex.ru", null);
+    ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "Petr", "Petrov", "89097865555", "petrpetrov@yandex.ru", null);
     app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() -1);
+    before.remove(before.size() - 1);
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
