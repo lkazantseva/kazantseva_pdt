0776c2d4c502f60f8b2e76600daa9404d05cfb2e
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-05-21 23:33:11 +0700

    Реализована загрузка информации о тестовом стенде из конфигурационного файла

--- addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/kaz/pdt/addressbook/tests/TestBase.java
@@ -1,13 +1,22 @@
 package ru.kaz.pdt.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.kaz.pdt.addressbook.appmanager.ApplicationManager;
 
-public class TestBase {
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
@@ -17,6 +26,16 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object [] p) {
+    logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
