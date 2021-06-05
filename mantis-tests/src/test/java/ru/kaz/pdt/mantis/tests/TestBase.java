b1f4b59b364935fecfdc79e633919c42b48e5d32
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-06-05 08:44:53 +0700

    проблема с импортом applicationManager

---
+++ mantis-tests/src/test/java/ru/kaz/pdt/mantis/tests/TestBase.java
@@ -0,0 +1,37 @@
+package ru.kaz.pdt.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.kaz.pdt.addressbook.appmanager.ApplicationManager;
+import ru.kaz.pdt.addressbook.model.ContactData;
+import ru.kaz.pdt.addressbook.model.Contacts;
+import ru.kaz.pdt.addressbook.model.GroupData;
+import ru.kaz.pdt.addressbook.model.Groups;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+
+}
