73d9a9c371866d283f61ea5a5000061d501bed92
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-06-07 17:15:03 +0700

    LoginTest Failure

--- mantis-tests/src/test/java/ru/kaz/pdt/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/kaz/pdt/mantis/appmanager/HttpSession.java
@@ -1,9 +1,15 @@
 package ru.kaz.pdt.mantis.appmanager;
 
-import org.omg.CORBA.NameValuePair;
-import org.openqa.selenium.remote.http.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
-import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +25,7 @@ public class HttpSession {
 
   public boolean login(String username, String password) throws IOException {
     HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
-    List<NameValuePair> params = new ArrayList<>();
+    List<BasicNameValuePair> params = new ArrayList<>();
     params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair("password", password));
     params.add(new BasicNameValuePair("secure_session", "on"));
@@ -29,7 +35,7 @@ public class HttpSession {
     String body = getTextFrom(response);
     return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
-  private String getTextFrom(CloseableHttpResponse response) {
+  private String getTextFrom(CloseableHttpResponse response) throws IOException {
     try {
       return EntityUtils.toString(response.getEntity());
     } finally {
