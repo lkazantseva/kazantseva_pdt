534fe2004d9c281cb27b0a6aa68e070b56a24829
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-06-10 00:47:32 +0700

    8.6 - RegistrationTests

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -4,8 +4,8 @@ $g_db_type                = 'mysqli';
 $g_database_name          = 'bugtracker';
 $g_db_username            = 'root';
 $g_db_password            = '';
-
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
 $g_signup_use_captcha     = OFF;
 $g_default_timezone       = 'Europe/Berlin';
-
+$g_smtp_host              = "localhost";
 $g_crypto_master_salt     = 'I9hUPUbYoJ6uMr0xB0aXMmsBmcnLQwCqYdb+s3JFLZY=';
