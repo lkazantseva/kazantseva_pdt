0c8795e3667e756f148a978e7d026ee3f679f8a4
lkazantseva <82151239+lkazantseva@users.noreply.github.com>
2021-06-17 12:01:30 +0700

    Реализована интеграция тестов с баг-трекером MantisBT

--- mantis-tests/src/test/java/ru/kaz/pdt/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/kaz/pdt/mantis/tests/TestBase.java
@@ -1,11 +1,20 @@
 package ru.kaz.pdt.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.kaz.pdt.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -22,6 +31,24 @@ public class TestBase {
   public void tearDown() throws Exception {
     app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = new MantisConnectLocator().
+            //getMantisConnectPort(new URL("http://localhost/mantisbt-2.25.1/api/soap/mantisconnect.php"));
+                    getMantisConnectPort(new URL(app.getProperty("mantis.port.URL")));
+    IssueData fixedIssueData = mc.mc_issue_get("administrator", "root", BigInteger.valueOf(issueId));
+    if (fixedIssueData.getResolution().getName().equals("fixed")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 
 }
